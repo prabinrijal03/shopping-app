30e598b1d8755de686a0fc96a096ef1c0eba935a
prabinrijal03 <prabinrijal03@gmail.com>
2024-11-13 22:31:46 +0545

    completed

--- lib/providers/cart_provider.dart
+++ lib/providers/cart_provider.dart
@@ -1,17 +1,12 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
 import 'package:riverpod_files/models/product.dart';
+part 'cart_provider.g.dart';
 
-class CartNotifier extends Notifier<Set<Product>> {
+@riverpod
+class CartNotifier extends _$CartNotifier {
   @override
   Set<Product> build() {
-    return const {
-      Product(
-          id: '4',
-          title: 'Red Backpack',
-          price: 14,
-          image: 'assets/products/backpack.png'),
-    };
+    return const {};
   }
 
   void addProduct(Product product) {
@@ -27,6 +22,12 @@ class CartNotifier extends Notifier<Set<Product>> {
   }
 }
 
-final cartNotifierProvider = NotifierProvider<CartNotifier, Set<Product>>(() {
-  return CartNotifier();
-});
+@riverpod
+int cartTotal(ref) {
+  final cartProducts = ref.watch(cartNotifierProvider);
+  int total = 0;
+  for (Product product in cartProducts) {
+    total += product.price;
+  }
+  return total;
+}
--- lib/screens/cart/cart_screen.dart
+++ lib/screens/cart/cart_screen.dart
@@ -15,6 +15,7 @@ class _CartScreenState extends ConsumerState<CartScreen> {
   @override
   Widget build(BuildContext context) {
     final cartProducts = ref.watch(cartNotifierProvider);
+    final total = ref.watch(cartTotalProvider);
     return Scaffold(
       appBar: AppBar(
         title: const Text('Your Cart'),
@@ -47,8 +48,8 @@ class _CartScreenState extends ConsumerState<CartScreen> {
                 );
               }).toList(), // output cart products here
             ),
-
-            // output totals here
+            Expanded(child: SizedBox()),
+            Text("Total price: Rs $total")
           ],
         ),
       ),
